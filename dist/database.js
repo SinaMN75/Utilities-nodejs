2499ccf51111a956456625c81f1b487c18511ebd
SinaMN75 <sina75mn@outlook.com>
2021-03-02 21:58:40 +0330

    :)

--- dist/database.js
+++ dist/database.js
@@ -1,29 +1,13 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectSequelize = exports.connectMongoose = void 0;
-const mongoose = __importStar(require("mongoose"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const sequelize_1 = require("sequelize");
 function connectMongoose(mongodbURI) {
-    mongoose.connect(mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose_1.default.connect(mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log(`mongodb connected`))
         .catch(() => console.log("error connecting to mongodb"));
 }
