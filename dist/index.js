de2da7f4b3e095a04876eea1d29e9aa63ff63df9
SinaMN75 <sina75mn@outlook.com>
2021-03-02 21:42:41 +0330

    :)

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.database = void 0;
+exports.database = require("./database");
 //# sourceMappingURL=index.js.map
