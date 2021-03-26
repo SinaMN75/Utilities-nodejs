dff0daef6ee29e23eaa7f46259e08fcf26a4c65b
SinaMN75 <sina75mn@outlook.com>
2021-03-26 17:32:19 +0430

    :)

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,1 @@
-/// <reference types="mongoose" />
-import express from "express";
-export declare function connectMongoDB(uri: string): void;
-export declare function encrypt(value: string, action: Function, error: Function): void;
-export declare function compareValues(password: string, hash: string, action: Function): void;
-export declare function notFound(response: express.Response): void;
-export declare const Price: import("mongoose").Model<import("./models/price").IPrice, {}>;
 //# sourceMappingURL=index.d.ts.map
