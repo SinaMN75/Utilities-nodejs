d7318ca63a1c5510a85505044c2af53450624eb3
SinaMN75 <sina75mn@outlook.com>
2021-03-26 16:56:20 +0430

    :)

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,11 @@
-export import express = require("express");
-export import encrypt = require("./general/encrypt");
-export import nosql = require("./database/nosql");
-export import expressExtension = require("./general/express");
+export import expressM = require("express");
+export import encryptM = require("./general/encrypt");
+export import nosqlM = require("./database/nosql");
+export import expressExtensionM = require("./general/express");
+export declare namespace u {
+    const express: typeof expressM;
+    const encrypt: typeof encryptM;
+    const nosql: typeof nosqlM;
+    const expressExtension: typeof expressExtensionM;
+}
 //# sourceMappingURL=index.d.ts.map
