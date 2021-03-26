167f2d7faca973211960548c991efc1735c47a8a
SinaMN75 <sina75mn@outlook.com>
2021-03-26 16:24:30 +0430

    :)

---
+++ lib/database/database_config.d.ts
@@ -0,0 +1,4 @@
+export declare const databaseConfig: {
+    mongoDBURI: string;
+};
+//# sourceMappingURL=database_config.d.ts.map
---
+++ lib/database/nosql.d.ts
@@ -0,0 +1,2 @@
+export declare function connectMongoDB(): void;
+//# sourceMappingURL=nosql.d.ts.map
