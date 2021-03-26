234ed45cc946e2f042b74cf17c40c41ed0542bfb
SinaMN75 <sina75mn@outlook.com>
2021-03-26 16:22:47 +0430

    :)

--- src/database/nosql.ts
+++ src/database/nosql.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
+import {databaseConfig} from "./database_config";
 
-export function connectMongoDB(uri: string) {
-	mongoose.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true})
+export function connectMongoDB() {
+	mongoose.connect(databaseConfig.mongoDBURI, {useUnifiedTopology: true, useNewUrlParser: true})
 	        .then(() => console.log(`connection to mongodb.`))
 	        .catch((e) => console.log(`connection to mongodb failed ${e.message}`));
 }
