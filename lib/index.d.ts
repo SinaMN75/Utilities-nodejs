d25ac745476f34e0997550d11f1583064350574f
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-11-21 21:16:28 +0330

    auto commit

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,5 @@
+export * from "./database/nosql";
+export * from "./general/express";
+export * from "./general/encrypt";
+export * from "./general/utils";
 //# sourceMappingURL=index.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "./database/nosql"
+export * from "./general/express";
+export * from "./general/encrypt";
+export * from "./general/utils";
