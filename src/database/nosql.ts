396c9ebc30cb45c0f4bbdde24eac10e91fd3d98a
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-11-22 12:46:49 +0330

    auto commit

--- src/database/nosql.ts
+++ src/database/nosql.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
+import {logError, logOk} from "../general/utils";
 
 export function connectMongoDB(uri: string) {
 	mongoose.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true})
-	        .then(() => console.log(`${uri} is running`))
-	        .catch((e) => console.log(`connection to mongodb failed ${e.message}`));
+	        .then(() => logOk(`${uri} is running`))
+	        .catch((e) => logError(`connection to mongodb failed ${e.message}`));
 }
