d04aff16b8ec65b8e09aaa378ed48e5c03b8d943
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-11-24 22:33:27 +0330

    auto commit

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,6 +3,7 @@ export import mongoose = require("mongoose");
 export import joi = require("joi");
 export import chalk = require("chalk");
 export import morgan = require("morgan");
+export import cors = require("cors");
 export declare const helmet: any;
 export * from "./database/nosql";
 export * from "./general/express";
