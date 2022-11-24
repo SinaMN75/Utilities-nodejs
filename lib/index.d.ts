6ee2cbc8e60e466e3499ab65dc5f7f113097c157
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-11-24 21:10:17 +0330

    auto commit

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,13 @@
 import ex from "express";
 import mongo from "mongoose";
+import jo from "joi";
 export * from "./database/nosql";
 export * from "./general/express";
 export * from "./general/encrypt";
 export * from "./general/utils";
 export declare const express: typeof ex;
 export declare const mongoose: typeof mongo;
+export declare const joi: jo.Root;
 export declare const helmet: any;
 export declare const chalk: any;
 export declare const morgan: any;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import ex from "express";
 import mongo from "mongoose";
+import jo from "joi";
 
 export * from "./database/nosql"
 export * from "./general/express";
@@ -8,6 +9,7 @@ export * from "./general/utils";
 
 export const express = ex;
 export const mongoose = mongo;
+export const joi = jo;
 export const helmet = require("helmet");
 export const chalk = require("chalk");
 export const morgan = require("morgan");
