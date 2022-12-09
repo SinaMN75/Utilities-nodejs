b7eacd977570f2210aa9509bd8f95e1217eec5d6
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-12-09 23:07:36 +0330

    auto commit

--- lib/general/express.d.ts
+++ lib/general/express.d.ts
@@ -1,3 +1,4 @@
-import express from "express";
+import express, { Express } from "express";
 export declare function notFound(response: express.Response): void;
+export declare function setupExpress(app: Express): void;
 //# sourceMappingURL=express.d.ts.map
--- src/general/express.ts
+++ src/general/express.ts
@@ -1,6 +1,16 @@
-import express, {Router} from "express";
+import express, {Express, Router} from "express";
+import {cors, helmet, morgan} from "../index";
 
 export function notFound(response: express.Response) {
 	response.status(404).json({message: "Not Found"});
 	const router: Router = express.Router();
+}
+
+export function setupExpress(app: Express) {
+	app.use(express.json());
+	app.use(express.urlencoded({extended: false}));
+	app.use(cors());
+	app.use(express.static("public"));
+	app.use(helmet());
+	app.use(morgan("dev"));
 }
