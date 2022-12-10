0945c9c3cb3d186cd75782b1e5adeae5c2289da7
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-12-10 10:01:26 +0330

    auto commit

--- lib/general/http_interceptor.d.ts
+++ lib/general/http_interceptor.d.ts
@@ -1,8 +1,7 @@
 import { Method } from "axios";
-export declare function request(method: Method, url: string, body: object | null, headers: any, onResponse: (response: any) => any, onError: (error: any) => any): Promise<void>;
-export declare function httpGet(url: string, headers: any | null, onResponse: (response: any) => any, onError: (response: any) => any): Promise<void>;
-export declare function httpPost(url: string, body: object | null | undefined, headers: any | null, onResponse: (response: any) => any, onError: (response: any) => any): Promise<void>;
-export declare function httpPostMultiPart(url: string, body: object | null | undefined, headers: any | null, onResponse: (response: any) => any, onError: (response: any) => any): Promise<void>;
-export declare function httpPut(url: string, body: object | null | undefined, headers: any | null, onResponse: (response: any) => any, onError: (response: any) => any): Promise<void>;
-export declare function httpDelete(url: string, headers: any | null, onResponse: (response: any) => any, onError: (response: any) => any): Promise<void>;
+export declare function request(method: Method, url: string, body: object | null, headers?: any): Promise<any>;
+export declare function httpGet(url: string, headers: any | null): Promise<any>;
+export declare function httpPost(url: string, body: object | null | undefined, headers: any | null): Promise<any>;
+export declare function httpPut(url: string, body: object | null | undefined, headers: any | null): Promise<any>;
+export declare function httpDelete(url: string, headers: any | null): Promise<any>;
 //# sourceMappingURL=http_interceptor.d.ts.map
--- src/general/http_interceptor.ts
+++ src/general/http_interceptor.ts
@@ -1,93 +1,30 @@
-import {AxiosResponse, Method} from "axios";
+import {Method} from "axios";
 
 const axios = require('axios');
 
-export async function request(method: Method,
-                              url: string,
-                              body: object | null,
-                              headers: any = null,
-                              onResponse: (response: any) => any,
-                              onError: (error: any) => any) {
-
-	await axios({
+export async function request(method: Method, url: string, body: object | null, headers: any = null) {
+	return await axios({
 		method: method,
 		url: url,
-        headers: headers,
+		headers: headers,
 		data: body,
 		responseType: "json",
 		withCredentials: false,
-	}).then(function (response: AxiosResponse) {
-		if (response.status >= 200 || response.status <= 299) onResponse(response.data);
-		else onError(response.data);
-		console.log(`${method} ${url}`, response.data);
-	}).catch(function (response: any) {
-		onError(response);
-		console.log(`${method} ${url}`, response.data);
-	}).then(function () {
-		console.log("WTF");
 	});
-
 }
 
-export async function httpGet(url: string,
-                              headers: any | null,
-                              onResponse: (response: any) => any,
-                              onError: (response: any) => any) {
-	return await request("get",
-		url,
-		null,
-		headers,
-		(response: any) => onResponse(response),
-		(response: any) => onError(response));
+export async function httpGet(url: string, headers: any | null) {
+	return await request("get", url, null, headers);
 }
 
-export async function httpPost(url: string,
-                               body: object | null = null,
-                               headers: any | null,
-                               onResponse: (response: any) => any,
-                               onError: (response: any) => any) {
-	return await request("post",
-		url,
-		body,
-		headers,
-		(response: any) => onResponse(response),
-		(response: any) => onError(response));
+export async function httpPost(url: string, body: object | null = null, headers: any | null) {
+	return await request("post", url, body, headers);
 }
 
-export async function httpPostMultiPart(url: string,
-                                        body: object | null = null,
-                                        headers: any | null,
-                                        onResponse: (response: any) => any,
-                                        onError: (response: any) => any) {
-	return await request("post",
-		url,
-		body,
-		headers,
-		(response: any) => onResponse(response),
-		(response: any) => onError(response));
+export async function httpPut(url: string, body: object | null = null, headers: any | null) {
+	return await request("put", url, body, headers);
 }
 
-export async function httpPut(url: string,
-                              body: object | null = null,
-                              headers: any | null,
-                              onResponse: (response: any) => any,
-                              onError: (response: any) => any) {
-	return await request("put",
-		url,
-		body,
-		headers,
-		(response: any) => onResponse(response),
-		(response: any) => onError(response));
-}
-
-export async function httpDelete(url: string,
-                                 headers: any | null,
-                                 onResponse: (response: any) => any,
-                                 onError: (response: any) => any) {
-	return await request("delete",
-		url,
-		null,
-		headers,
-		(response: any) => onResponse(response),
-		(response: any) => onError(response));
+export async function httpDelete(url: string, headers: any | null) {
+	return await request("delete", url, null, headers);
 }
