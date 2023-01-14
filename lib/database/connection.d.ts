4ad37212bbd0c3892c681b9c4798b4a4c6fa1f53
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-01-14 15:29:09 +0330

    auto commit

--- lib/database/connection.d.ts
+++ lib/database/connection.d.ts
@@ -1,5 +1,5 @@
 import { Dialect, Sequelize } from "sequelize";
 export declare const connectMongoDB: (uri: string) => void;
-export declare const sequelizeDatabase: (database: string, username: string, password: string, dialect: Dialect) => Sequelize;
-export declare function connectToSequelize(database: string, username: string, password: string, dialect: Dialect): void;
+export declare const sequelizeDatabase: (host: string, database: string, username: string, password: string, dialect: Dialect) => Sequelize;
+export declare function connectToSequelize(host: string, database: string, username: string, password: string, dialect: Dialect): void;
 //# sourceMappingURL=connection.d.ts.map
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -5,15 +5,16 @@ export const connectMongoDB = (uri: string) => {
 	mongoose.connect(uri, () => logOk(`${uri} is running`));
 };
 
-export const sequelizeDatabase = (database: string, username: string, password: string, dialect: Dialect): Sequelize => {
+export const sequelizeDatabase = (host: string, database: string, username: string, password: string, dialect: Dialect): Sequelize => {
 	require('sequelize-hierarchy-fork')(Sequelize);
 	return new sequelize.Sequelize(database, username, password, {
+		host: host,
 		dialect: dialect,
 	});
 };
 
-export function connectToSequelize(database: string, username: string, password: string, dialect: Dialect) {
-	sequelizeDatabase(database, username, password, dialect).authenticate()
+export function connectToSequelize(host: string, database: string, username: string, password: string, dialect: Dialect) {
+	sequelizeDatabase(host, database, username, password, dialect).authenticate()
 	                                                        .then(() => console.log(`connected to ${database} DB`))
 	                                                        .catch(() => console.log(`connection to ${database} DB failed`));
 }
